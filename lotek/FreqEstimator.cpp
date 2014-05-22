933fd499cb707ed94596a0cbb2b2a2cd9b810eb5
john <jbrzusto@fastmail.fm>
2014-05-22 19:24:37 +0000

    decent frequency estimator based on pairwise bin power ratios; power estimate is wonky

--- lotek/FreqEstimator.cpp
+++ lotek/FreqEstimator.cpp
@@ -20,11 +20,13 @@
 #define fftw_free(X) fftwf_free(X)
 #endif
 
-FreqEstimator::FreqEstimator (unsigned max_frames, unsigned channels) :
+FreqEstimator::FreqEstimator (int max_frames, int channels) :
     m_max_frames(max_frames),
     m_channels(channels),
     m_first_zero_frame(max_frames), // indicate input float array needs zeroing
-    m_freq_scale(1.0 / max_frames)
+    m_freq_scale(1.0 / max_frames),
+    m_have_window(false),
+    m_power_scale(1.0 / sqrtf(max_frames))
 {
 
     if (! m_fftw_wisdom_loaded) {
@@ -53,6 +55,8 @@ FreqEstimator::FreqEstimator (unsigned max_frames, unsigned channels) :
     default:
         throw std::runtime_error("FreqEstimator: channels must be 1 or 2");
     }
+    //    generateWindowingCoefficients();
+    calculateRatioMaps();
 }
 
 FreqEstimator::~FreqEstimator()
@@ -71,15 +75,20 @@ FreqEstimator::~FreqEstimator()
 
 #define Pwr(x) (((float *)(&x))[0] * ((float *)(&x))[0] + ((float *)(&x))[1] * ((float *)(&x))[1])
 
-float FreqEstimator::get (int16_t *samples, unsigned frames, float *pwr_out)
+float FreqEstimator::get (int16_t *samples, int frames, float *pwr_out)
 {
-    unsigned i, j = m_channels * std::min(frames, m_max_frames);
+    int i, j = m_channels * std::min(frames, m_max_frames);
     float *input = (float *) m_input;
 
     // convert samples from S16_LE to float
 
-    for (i = 0; i < j; ++i)
-        input[i] = samples[i];
+    if (m_have_window) {
+        for (i = 0; i < j; ++i)
+            input[i] = samples[i] * m_window[i / 2];
+    } else {
+        for (i = 0; i < j; ++i)
+            input[i] = samples[i];
+    }        
     
     // pad with zeroes, if necessary
 
@@ -96,7 +105,7 @@ float FreqEstimator::get (int16_t *samples, unsigned frames, float *pwr_out)
 
     // find the max power bin
             
-    unsigned bin_high = m_fft_bins;
+    int bin_high = m_fft_bins;
             
     float max_power = 0.0;
     int max_bin = -1;
@@ -108,59 +117,182 @@ float FreqEstimator::get (int16_t *samples, unsigned frames, float *pwr_out)
         }
     }
             
-    // use a quadratic estimator to find the peak frequency estimate using
-    // 3 bins that include the max power bin.  These will be numbered
-    // win - 1, win, win+1, and win + 2. We avoid the DC bin, and
-    // don't overstep the array
+    float prevPwr, nextPwr;
 
-    // FIXME: what if max_bin == 1 or max_bin == m_fft_bin - 1 ?
-    int win = max_bin;
+    if (max_bin > 1) {
+        prevPwr = Pwr(m_output[max_bin - 1]);
+    } else {
+        prevPwr = 0;
+    }
+    if (max_bin >= bin_high) {
+        nextPwr = 0;
+    } else {
+        nextPwr = Pwr(m_output[max_bin + 1]);
+    }
 
-    if (win == 1)
-        ++ win;
-    else if (win + 1 >= m_fft_bins)
-        -- win;
 
-    // get the power values in the window of 3 bins, for peak estimation
-    // apparently, this works better in log units
+    float bin_offset = estimateBinOffset(prevPwr, max_power, nextPwr);
 
-    float pwr[3] = {log10f(Pwr(m_output[win - 1])),
-                    log10f(Pwr(m_output[win + 0])),
-                    log10f(Pwr(m_output[win + 1]))};
-
-    // this is the quadratic estimate of peak location given the three values
-    // computed above.
-
-    float bin_offset = (pwr[0] - pwr[2]) / (2.0* (pwr[2] - 2.0 * pwr[1] + pwr[0]));
-
-    // interpolate the quadratic estimate.
+    // sum the energy in the three bins, then scale
     if (pwr_out)
-        *pwr_out = exp10f(pwr[1] - (pwr[0]-pwr[2]) * (pwr[0] - pwr[2]) / (8.0 * (pwr[2]-2.0 * pwr[1]+pwr[0]))) / frames;
+        *pwr_out = 10 * log10f((prevPwr + max_power + nextPwr) * m_power_scale / 32767 / 32767);
  
     // win + bin_offset is the estimate of the (possibly non-integer) bin with the highest
     // power.  We convert that to an estimate in cycles per sample, keeping in mind that
     // the data were zero-padded out to m_max_frames
 
-    if (win + bin_offset > m_fft_bins / 2)
-        return - (m_fft_bins - win - bin_offset) * m_freq_scale;
+    float bin_est = max_bin + bin_offset;
 
-    return (win + bin_offset) * m_freq_scale;
+    if (bin_est > m_fft_bins / 2)
+        // frequencies above 1/2 Nyquist are considered negative frequencies
+        bin_est = - (m_fft_bins - bin_est);
+
+    return bin_est * m_freq_scale;
 }
 
 void
-FreqEstimator::generateWindowingCoefficients(int N, std::vector < float > &window, float &win_sum, float &win_sumsq)
+FreqEstimator::generateWindowingCoefficients()
 {
     // generate a Hamming window of size N in float array window, 
     // Return sums and sums of squares of window coefficients in win_sum and win_sumsq.
 
-    window = std::vector < float > (N);
-    win_sum = win_sumsq = 0.0;
-    for (int i = 0; i < N; ++i) {
-        window[i] = 0.54 - 0.46 * cosf(2 * M_PI * i / (N - 1));
-        win_sum += window[i];
-        win_sumsq += window[i] * window[i];
+    m_window = std::vector < float > (m_max_frames);
+    m_win_sum = m_win_sumsq = 0.0;
+    for (int i = 0; i < m_max_frames; ++i) {
+        m_window[i] = 0.54 - 0.46 * cosf(2 * M_PI * i / (m_max_frames - 1));
+        m_win_sum += m_window[i];
+        m_win_sumsq += m_window[i] * m_window[i];
     }
+    m_have_window = true;
+    m_power_scale = 1.0 / (sqrtf(m_max_frames) * (m_win_sum * m_win_sum / 2));
 }
+
+float
+FreqEstimator::binRatio(float d)
+{
+    // ratio of power in adjacent bins for a pure tone of given offset
+    // frequency (in units of bins) from being an exact bin.
+
+    // Consider the DFT of exp(2 \pi \i f i / n) on the sequence i = 0, 1, ..., n - 1
+
+    // if f = m + d where m is an integer in 2..n -2 and abs(d) <= 0.5 then 
+    // this function returns Pow (m - 1) ^2 / Pow ( m ) ^2. 
+    // Pow(m) is the power in bin m.
+    // If -0.5 <= d <= 0, the function returns the ratio [max(Pow(m-1), Pow(m+1)) / Pow(m)]^2
+    // If 0 <= d <= 0.5, the function returns the ratio [min(Pow(m-1), Pow(m+1)) / Pow(m)]^2
+    // In particular, the function returns 0 for d = 0 and 1 for d = -0.5
+
+    double t1, t2, t3, t31, t32, t4, t5, t51, t6, t7, t72, t8, t82;
+    if (fabsf(d) < 1.0e-5)
+        return 0.0;
+
+    t1 = 1.0 / m_max_frames;
+    t2 = 2.0 * M_PI * d * t1;
+    t3 = cos(t2);
+    t31 = t3 - 1.0;
+    t32 = sin(t2);
+    t4 = 2.0 * M_PI * (d + 1.0) * t1;
+    t5 = cos(t4);
+    t51 = t5 - 1.0;
+    t6 = 2.0 * M_PI * d;
+    t7 = cos(t6);
+    t72 = sin(t6);
+    t8 = t72 * t72 + t7 * t7 - 2.0 * t7 + 1.0;
+    t82 = sin(t4);
+    return t31 * t31 * t8 * (t82 * t82 +t5 * t5 - 2.0 * t5 + 1.0) / (t51 * t51 * t8 * (t32 * t32 + t3 * t3 - 2.0 * t3 + 1.0));
+
+};
+
+
+float 
+FreqEstimator:: estimateBinOffset(float pwrPrev, float pwrMax, float pwrNext)
+{
+    // pwrMax: the power in the bin where a local maximum occurs
+    // pwrPrev: the power in the preceding bin, if that is not the DC bin, else -1
+    // pwrNext: the power in the following bin, if that exists, else -1
+    // at most one of pwrPrev and pwrNext can be -1; other parameters must be non-negative
+
+    if (pwrPrev <= 0.0)
+        return m_ratio2_to_offset [pwrNext / pwrMax * m_ratioMapScale2];
+    if (pwrNext <= 0.0)
+        return m_ratio2_to_offset [pwrPrev / pwrMax * m_ratioMapScale2];
+    
+    // in the general case, we get ratios on both sides, with each giving an offset.
+    // we average these.
+
+    float off1, off2;
+    if (pwrPrev >= pwrNext) { 
+        // prev is 2nd highest bin; offset is positive
+        off1 = m_ratio2_to_offset [pwrPrev / pwrMax * m_ratioMapScale2];
+        off2 = m_ratio3_to_offset [pwrNext / pwrMax * m_ratioMapScale3];
+        return - (off1 + off2) / 2;
+    } else {  
+        // next is 2nd highest bin; offset is positive
+        off1 = m_ratio2_to_offset [pwrNext / pwrMax * m_ratioMapScale2];
+        off2 = m_ratio3_to_offset [pwrPrev / pwrMax * m_ratioMapScale3];
+        return (off1 + off2) / 2;
+    }
+};
+
+void
+FreqEstimator::calculateRatioMaps(float prec) 
+{
+    // invert the binRatio function to a given precision, on each branch
+    // m_ratio2_to_offset will map [0,1] -> [0, 0.5] using the negative side of binRatio()
+    // m_ratio3_to_offset will map [0,1] -> [0, 0.3334095 ] using the positive side of binRatio()
+
+    // we use bisection on this monotonic function to search for inverse values
+
+    int n1 = binRatio(-0.5) / prec;
+    int n2 = binRatio( 0.5) / prec;
+    
+    m_ratioMapScale2 = n1 / binRatio(-0.5);
+    m_ratioMapScale3 = n2 / binRatio(0.5);
+
+    m_ratio2_to_offset = std::vector < float > (n1);
+    m_ratio3_to_offset = std::vector < float > (n2);
+
+    int i;
+    float y = 0;
+    for (i = 0; i < n1; ++i) {
+        float ylo = y, yhi = 0.5;
+        for (;;) {
+            float diff = binRatio(-y) - i / m_ratioMapScale2;
+            if (fabsf(diff) < prec / 10)
+                break;
+            if (diff < 0.0) {
+                ylo = y;
+                y = (yhi + y) / 2;
+            } else {
+                yhi = y;
+                y = (ylo + y) / 2;
+            }
+        }
+        m_ratio2_to_offset[i] = y;
+    }
+
+    y = 0;
+    for (i = 0; i < n2; ++i) {
+        float ylo = y, yhi = 0.5;
+        for (;;) {
+            float diff = binRatio(y) - i / m_ratioMapScale3;
+            if (fabsf(diff) < prec / 10)
+                break;
+            if (diff < 0.0) {
+                ylo = y;
+                y = (yhi + y) / 2;
+            } else {
+                yhi = y;
+                y = (ylo + y) / 2;
+            }
+        }
+        m_ratio3_to_offset[i] = y;
+    }
+
+};
+    
+std::vector < float > m_ratio2_to_offset; // lookup table converting ratio (of 2nd bin to max) to offset
+std::vector < float > m_ratio3_to_offset; // lookup table converting ratio (of 3rd bin to max) to offset
 
 const char * FreqEstimator::m_fftw_wisdom_filename = "./fftw_wisdom.dat";
 bool FreqEstimator::m_fftw_wisdom_loaded = false;
--- lotek/FreqEstimator.h
+++ lotek/FreqEstimator.h
@@ -1,11 +1,11 @@
 /* -*- mode:c++; c-basic-offset: 4 indent-tabs-mode: nil -*-  vi:set ts=8 sts=4 sw=4: */
 
 /*
-    FreqEstimator.h - estimate main frequency of a pulse from raw audio data.  FIXME: hardcoded S16_LE
+  FreqEstimator.h - estimate main frequency of a pulse from raw audio data.  FIXME: hardcoded S16_LE
 
-    Copyright 2014 John Brzustowski
+  Copyright 2014 John Brzustowski
 
-    License: GPL v 2.0 or later.
+  License: GPL v 2.0 or later.
 
 */
 
@@ -23,47 +23,64 @@
 // don't need to reallocate storage.  This is NOT thread safe!
 
 struct cachedFFTWPlan {
-  fftwf_complex *m_input; // for 1 channel: samples stored as floats; for 2 channels: floating point samples, as interleaved I/C pairs
-  fftwf_complex *m_output; // DFT output from pulse samples
-  fftwf_plan m_plan; // FFT plan for I/Q sample
+    fftwf_complex *m_input; // for 1 channel: samples stored as floats; for 2 channels: floating point samples, as interleaved I/C pairs
+    fftwf_complex *m_output; // DFT output from pulse samples
+    fftwf_plan m_plan; // FFT plan for I/Q sample
 };
 
 class FreqEstimator {
 
- public:
+public:
 
-  // constructor.  max_frames is the largest number of frames of data
-  // allowed (typically made large to allow zero-padding for better
-  // frequency resolution) channels is the number of channels, 1 or 2
+    // constructor.  max_frames is the largest number of frames of data
+    // allowed (typically made large to allow zero-padding for better
+    // frequency resolution) channels is the number of channels, 1 or 2
 
-  FreqEstimator (unsigned max_frames, unsigned channels=2);
+    FreqEstimator (int max_frames, int channels=2);
 
-  ~FreqEstimator();
+    ~FreqEstimator();
 
-  // estimate the frequency with maximum power, and (if pwr_out != NULL)
-  // return an estimate of its power in *pwr_out
-  // the estimate is in cycles per sample, so the caller has to 
-  // multiply by the sampling rate to get frequency in Hz.
-  float get (int16_t *samples, unsigned frames, float *pwr_out = 0);
+    // estimate the frequency with maximum power, and (if pwr_out != NULL)
+    // return an estimate of its power in *pwr_out
+    // the estimate is in cycles per sample, so the caller has to 
+    // multiply by the sampling rate to get frequency in Hz.
+    float get (int16_t *samples, int frames, float *pwr_out = 0);
 
- protected:
+    void generateWindowingCoefficients();
 
-  unsigned m_max_frames;
-  unsigned m_channels;
-  unsigned m_fft_bins;
+protected:
 
-  unsigned m_first_zero_frame; // index of first frame in m_ff
-  float m_freq_scale;  // amount to multiply a bin index to get cycles per sample
-  static const char * m_fftw_wisdom_filename;
-  static bool m_fftw_wisdom_loaded;
+    int m_max_frames;
+    int m_channels;
+    int m_fft_bins;
 
-  fftwf_complex *m_input; // for 1 channel: samples stored as floats; for 2 channels: floating point samples, as interleaved I/C pairs
-  fftwf_complex *m_output; // DFT output from pulse samples
-  fftwf_plan m_plan; // FFT plan for I/Q sample
+    int m_first_zero_frame; // index of first frame in m_ff
+    float m_freq_scale;  // amount to multiply a bin index to get cycles per sample
+    static const char * m_fftw_wisdom_filename;
+    static bool m_fftw_wisdom_loaded;
 
-  static void generateWindowingCoefficients(int N, std::vector < float > &window, float &win_sum, float &win_sumsq);
+    fftwf_complex *m_input; // for 1 channel: samples stored as floats; for 2 channels: floating point samples, as interleaved I/C pairs
+    fftwf_complex *m_output; // DFT output from pulse samples
+    fftwf_plan m_plan; // FFT plan for I/Q sample
 
-  static std::map < std::pair < int, short >, cachedFFTWPlan > m_cached_plans;
+    bool m_have_window;
+    std::vector < float > m_window;
+    float m_win_sum;
+    float m_win_sumsq;
+    float m_power_scale;
+
+    float binRatio(float d); // ratio of spectral power in bin x+d to bin x+d+1 for the function exp(2*pi*x*t)
+    float estimateBinOffset(float pwrPrev, float pwrMax, float pwrNext); // estimate bin offset (from 0) given power at max and previous
+    
+    std::vector < float > m_ratio2_to_offset; // lookup table that inverts binRatio function
+    std::vector < float > m_ratio3_to_offset; // lookup table that inverts binRatio function
+
+    float m_ratioMapScale2;
+    float m_ratioMapScale3;
+
+    void calculateRatioMaps(float prec = 0.001); // set up ratio to offset lookup tables
+
+    static std::map < std::pair < int, short >, cachedFFTWPlan > m_cached_plans;
 };
 
 
